0dd7f5744763b6ebcc39246f416954e7e3ec7f80
drikadev <adriguti.adriana@hotmail.com>
2022-11-17 23:16:02 -0300

    atualização de css

--- src/Components/MyProjects/index.jsx
+++ src/Components/MyProjects/index.jsx
@@ -56,70 +56,70 @@ export default function MyProjects() {
                     // }}
                 >
                     <div>
-                        <h2>Projeto: Desafio ODS Jaçanã</h2>
+                        <h2>Desafio ODS Jaçanã</h2>
                         <img src={ODS} alt="Foto do ODS Jaçanã"/>
                         <p><a href="https://odsnojacana.netlify.app/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/Pratica-React-15-ODS-Jacana/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: API Harry Potter</h2>
+                        <h2>API Harry Potter</h2>
                         <img src={Harry} alt="Foto desafio Harry Potter"/>
                         <p><a href="https://apidoharrypotter.netlify.app/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/Pratica-React-13-API-HarryPotter/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: 1º Trabalho em dupla</h2>
+                        <h2>1º Trabalho em dupla</h2>
                         <img src={Dupla} alt="Foto projeto em dupla"/>
                         <p><a href="https://desafiopairprogramming.netlify.app/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/VaiNaWeb-React-pair-programming/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: To Do List</h2>
+                        <h2>To Do List</h2>
                         <img src={Lista} alt="Foto desafio todolist"/>
                         <p><a href="https://todolistcomsetstate.netlify.app" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/Pratica-React-11-ToDoList-com-useState/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: API de Filmes</h2>
+                        <h2>API de Filmes</h2>
                         <img src={Movies} alt="Foto desafio Filmes"/>
                         <p><a href="https://drikasmovies.netlify.app/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/Desafio-10-Drikas-Movies-APIs/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: 1º Trabalho Freelancer</h2>
+                        <h2>1º Trabalho Freelancer</h2>
                         <img src={Noivinhos} alt="Foto Noivinhos da Carminha"/>
                         <p><a href="https://drikadev.github.io/E-commerce-Noivinhos-da-Carminha/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/E-commerce-Noivinhos-da-Carminha/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: Legue Of Legends</h2>
+                        <h2>Legue Of Legends</h2>
                         <img src={LOL} alt="Foto Landing Page League Of Legends"/>
                         <p><a href="https://drikadev.github.io/Desafio-Login-League-of-Legends/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/Desafio-Login-League-of-Legends/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: Convite de Aniversário</h2>
+                        <h2>Convite de Aniversário</h2>
                         <img src={Convite} alt="Foto do Convite"/>
-                        <p><a href="https://drikadev.github.io/Convite.aniversario/" target="_blank">Clique aqui para ver o site</a></p>
-                        <p><a href="https://github.com/DrikaDev/Convite.aniversario/" target="_blank">Clique aqui para ver o repositório</a></p>
+                        <p><a href="https://drikadev.github.io/Convite-de-aniversario-responsivo/" target="_blank">Clique aqui para ver o site</a></p>
+                        <p><a href="https://github.com/DrikaDev/Convite-de-aniversario-responsivo" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: Decodificador Alura</h2>
+                        <h2>Decodificador Alura</h2>
                         <img src={Decofidicador} alt="Foto do Decodificador Alura"/>
                         <p><a href="https://drikadev.github.io/ChallengeDecodificador2/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/ChallengeDecodificador2/" target="_blank">Clique aqui para ver o repositório</a></p>
                     </div>
 
                     <div>
-                        <h2>Projeto: Desenhando o Finn com CSS</h2>
+                        <h2>Desenhando o Finn com CSS</h2>
                         <img src={Finn} alt="Foto desenho do Finn"/>
                         <p><a href="https://drikadev.github.io/Desafio10.Finn/" target="_blank">Clique aqui para ver o site</a></p>
                         <p><a href="https://github.com/DrikaDev/Desafio10.Finn/" target="_blank">Clique aqui para ver o repositório</a></p>
